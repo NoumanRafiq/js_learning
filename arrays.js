985f89794bee6d19ce0fafd0a89a3d292b34e60e
NoumanRafiq <nouman.rafiq25@gmail.com>
2026-07-16 14:16:13 +0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+// introduction to arrays
+
+let fruits = ['apple', 'orange', 'mango'];
+
+// push and pop from the end
+// fruits.push('newFruit');
+// console.log(fruits);
+// let poppedFruit = fruits.pop();
+// console.log(poppedFruit);
+// // let pushFruit = fruits.push('fruit');
+// console.log(fruits);
+
+// push and pop at start
+
+// unshift
+let startFruit = fruits.unshift('start fruit');
+console.log(fruits);
+
+let shiftFruit = fruits.shift();
+console.log(shiftFruit);
+console.log(fruits);
+
+
