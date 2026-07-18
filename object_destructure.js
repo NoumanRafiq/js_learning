21b4fbcfd24649faa1551a5b0ae4707fbb0f79ce
NoumanRafiq <nouman.rafiq25@gmail.com>
2026-07-18 14:51:52 +0500

    objects;

---
+++ object_destructure.js
@@ -0,0 +1,2 @@
+// object(maps) destructuring
+
--- objects.js
+++ objects.js
@@ -1,19 +1,38 @@
-const person_data = {
-    name: 'ali',
-    age: 22,
-    hobbies: ['programming', 'football', 'foodunist'],
+// const person_data = {
+//     name: 'ali',
+//     age: 22,
+//     hobbies: ['programming', 'football', 'foodunist'],
+// }
+
+// person_data['bg'] = 'A+';
+
+// // console.log(person_data);
+
+// for (let key in person_data){
+//     console.log(`${key} ->>> ${person_data[key]}`);
+// }
+// console.log('\n');
+
+// for (let key of Object.keys(person_data)){
+//     console.log(key,"-",person_data[key]);  
+//     // console.log(`${key} ->>> ${person_data[key]}`);
+// }
+
+const obj1 = {
+    key1: 'val1',
+    key2: 'val2',
+    key3: "thrid modified",
+    key4: 'fourth modified'
+};
+const obj2 = {
+    key1: 'Unique1st',
+    key2:'Unique2nd',
+    key3: 'val3',
+    key4: 'val4',
+};
+
+const newObj = {
+    ...obj1, ...obj2
 }
-
-person_data['bg'] = 'A+';
-
-// console.log(person_data);
-
-for (let key in person_data){
-    console.log(`${key} ->>> ${person_data[key]}`);
-    
-}
-
-console.log(Object.keys(person_data));
-
-
+console.log(newObj);
 // console.log(person_data.hobbies);
