5aea7599928753f419d2526d73b10962606f623e
NoumanRafiq <nouman.rafiq25@gmail.com>
2026-07-19 15:57:04 +0500

    objects destructuring techniques

--- object_destructure.js
+++ object_destructure.js
@@ -1,2 +1,15 @@
 // object(maps) destructuring
 
+const user = {
+    name: 'Ali',
+    age: 23,
+    education: 'hssc',
+    qualification: 'fsc'
+};
+
+let {name, age, ...newObj} = user;
+
+console.log(name);
+console.log(age);
+console.log(newObj);
+
