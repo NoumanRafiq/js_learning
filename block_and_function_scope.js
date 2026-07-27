96e9289e1c94b07eb604b4a156caeaba22adec28
NoumanRafiq <nouman.rafiq25@gmail.com>
2026-07-27 15:18:53 +0500

    block vs function socpe

---
+++ block_and_function_scope.js
@@ -0,0 +1,13 @@
+// in this program we will study about the block scope vs function scope
+// of let, var & const
+
+{
+    let myName = 'Nouman';
+}
+
+{
+    var myName = 'Ali';
+}
+{
+    const myName = 'Ahmed';
+}
