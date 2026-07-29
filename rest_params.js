de27ac324898e4951c5639edbe5864b9c816eaa0
NoumanRafiq <nouman.rafiq25@gmail.com>
2026-07-29 16:14:29 +0500

    rest parameters

--- rest_params.js
+++ rest_params.js
@@ -1,13 +1,13 @@
 
-function sumOfAll (...c){
-    let total;
-    for(let number of c){
+function sumOfAll (...numbers){
+    let total = 0;
+    for(let number of numbers){
         total = total + number;
     }
-    console.log('a, b, c', a, b, c);
+    // console.log('a, b, c', a, b, c);
     return total;
 }
 
-const answer = sumOfAll(1,2,3);
+const answer = sumOfAll(4,1,2,3);
 console.log(answer);
 
